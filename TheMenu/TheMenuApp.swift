c60e1b2278877241646d0c1783982cf7df06e422
Ewiniar <luo1172837419@icloud.com>
2023-05-30 16:27:29 -0500

    Initial Commit

---
+++ TheMenu/TheMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMenuApp.swift
+//  TheMenu
+//
+//  Created by 罗贤甫 on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
